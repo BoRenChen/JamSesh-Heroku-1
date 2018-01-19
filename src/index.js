3bbd6daa5e48b29768b366a5c1a20943d6a5c405
seanHolahan <holahan.s@husky.neu.edu>
2018-01-19 13:19:59 -0500

    first

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const socket =io();
+
+//socket test
+socket.on('hello', ({ message }) =>
+	alert(message));
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
